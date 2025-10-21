c6de8795f983ec0e5406f77ded537709c745130c
Diaki - Gammelgaard <crafting@borkgammelgaard.dk>
2025-10-21 14:06:46 +0200

    Ooga booga
    Springing all over the place

---
+++ app/src/main/java/p3project/app/App.java
@@ -0,0 +1,19 @@
+package p3project.app;
+
+import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+		System.out.println("Hello bish");
+	}
+
+}
