160b70199e69d6890c3bd0bc24e98c53f8692196
Crafting137 <crafting@borkgammelgaard.dk>
2025-10-31 13:06:59 +0100

    Linked to backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [count2, setCount2] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="http://localhost:8080/dockers" target="_blank">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+            </div>
+            <h1>Vite + localhost:8080/dockers</h1>
+            <div className="card">
+                <button
+                    onClick={() => {
+                        setCount((count) => count + 1);
+                        setCount2((count2) => count2 + 1);
+                    }}
+                >
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+                <div>
+                    <p>
+                        Hello World! This is a sample React application using
+                        Vite as the build tool.
+                    </p>
+                    <button onClick={() => setCount2((count2) => count2 + 1)}>
+                        Buttons clicked {count2} times!
+                    </button>
+                </div>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
-export default App
+export default App;
