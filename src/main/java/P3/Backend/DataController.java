987469e7a6b01be47c9117f01c1ae2fa5f4e6b3e
Crafting137 <crafting@borkgammelgaard.dk>
2025-12-02 12:39:59 +0100

    Refactor severity assessment logic
    Updated SeverityCalculator to separate single error severity assessment into its own method and refactored assessSeverity to process only Map entries. This prepares the code for future expansion of severity logic and improves code clarity.

--- src/main/java/P3/Backend/DataController.java
+++ src/main/java/P3/Backend/DataController.java
@@ -104,6 +104,7 @@ Database database = new Database();
         Map<String, Object> diagnosticsErrors = database.getDiagnosticsErrors(timeFrame).toMap();
         // Create an object to asses the severity of the error messages
         SeverityCalculator severityCalculator = new SeverityCalculator();
+        
         //Return the updated map of error messages with severities assigned
         return severityCalculator.assessSeverity(diagnosticsErrors);
     }
